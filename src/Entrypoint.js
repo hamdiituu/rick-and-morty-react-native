22864d557ac0b50f0300540e9fe64f97179bb7a6
Hamdi Tuğ <hamditugmobil@gmail.com>
2021-06-14 13:47:29 +0300

    status bar color change

--- src/Entrypoint.js
+++ src/Entrypoint.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import AppNavigation from './navigation';
 import {Provider} from 'react-redux';
 import store from './store';
 const Entrypoint = () => {
   return (
     <Provider store={store}>
+      <StatusBar backgroundColor={'#f2f2f2'} barStyle={'dark-content'} />
       <AppNavigation />
     </Provider>
   );
